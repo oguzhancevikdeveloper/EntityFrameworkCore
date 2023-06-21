34117cad586b0c0568f93c1b8d1b9773eb06f0b1
Oguzhan Cevik <oguzhan.cevik@FrostlineGames.onmicrosoft.com>
2023-06-21 09:23:31 +0300

    Add project files.

---
+++ EntityFrameworkCore/Program.cs
@@ -0,0 +1,10 @@
+﻿
+
+using EntityFrameworkCore.Models;
+ MasterContext _context = new();
+
+foreach (var category in _context.Categories)
+{
+    Console.WriteLine(category.CategoryName+"  :  "+category.Description);
+}
+Console.ReadLine();
